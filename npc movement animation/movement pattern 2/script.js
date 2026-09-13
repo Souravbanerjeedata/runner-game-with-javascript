6a219efca688e2f1c2aebe4f60da50fd459620e2
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-13 13:46:21 +0530

    enemy 4th movement pattern

--- npc movement animation/movement pattern 2/script.js
+++ npc movement animation/movement pattern 2/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
-const numberOfEnemies = 10;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -11,27 +11,23 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "../assets/enemy3.png";
+    this.image.src = "../assets/enemy2.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
-    this.width = this.spriteWidth / 2;
-    this.height = this.spriteHeight / 2;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = Math.random() * 500;
-    this.angleSpeed = Math.random() * 0.5 + 0.5;
-    // this.curve = Math.random() * 200 + 50;
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7;
   }
   update() {
-    this.x =
-      (canvas.width / 2) * Math.cos((this.angle * Math.PI) / 180) +
-      (canvas.width / 2 - this.width / 2);
-    this.y =
-      (canvas.height / 2) * Math.sin((this.angle * Math.PI) / 200) +
-      (canvas.height / 2 - this.height / 2);
+    this.x -= this.speed;
+    this.y += this.curve * Math.sin(this.angle);
     this.angle += this.angleSpeed;
     if (this.x + this.width < 0) this.x = canvas.width;
     // Animate sprites
